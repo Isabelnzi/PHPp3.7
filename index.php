73eb9287c1dfaa1d61607fe20da67eec4813d1e1
Nzingani <isabelnzingani@outlook.com>
2018-07-27 11:28:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P3ex7</title>
+  </head>
+  <body>
+    <?php
+    $not = 20;
+    while ($not < 100){
+    echo 'On tient le bon bout.';
+    $not = $not+ $not+15;//$not+= 15
+    }
+  ?>
+</body>
+</html>
